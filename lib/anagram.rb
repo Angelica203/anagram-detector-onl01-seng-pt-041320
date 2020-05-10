6ef616b12c0182f6290ccedb6c4f56724ebfa96e
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-10 19:19:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,7 @@ class Anagram
    @word = word
  end
 
- def match
-   (array_of_words)
+ def match(array_of_words)
    array_of_words.select do |element|
      (@word.split("").sort) == (element.split("").sort)
    end
